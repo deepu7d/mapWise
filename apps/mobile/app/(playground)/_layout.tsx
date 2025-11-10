5f4a5427c1eaf9dda8c5da64bcb4b50209aa10dc
deepu7d <deepanshu.9dd@gmail.com>
2025-11-10 17:30:04 +0530

    working chat

--- apps/mobile/app/(playground)/_layout.tsx
+++ apps/mobile/app/(playground)/_layout.tsx
@@ -1,36 +1,53 @@
 import React from "react";
 import { Tabs } from "expo-router";
+import { SocketProvider } from "@repo/hooks";
+import { sessionData } from "@repo/types";
+import { StoreProvider } from "@repo/store";
 export default function TabLayout() {
+  const sessionData: sessionData = {
+    roomId: "cmht0yeau0000m6rloumdy1iw",
+    userId: "cmht0z3ox0004m6rljv6em956",
+    username: "mobile",
+    destinationPosition: [30.357234, 76.7951835],
+    destinationName: "Jandli, Ambala Cantt, Ambala, Haryana, 132004, India",
+  };
   return (
-    <Tabs
-      initialRouteName="map"
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: "#bfdbfe",
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="map"
-        options={{
-          title: "Map",
-        }}
-      />
-      <Tabs.Screen
-        name="users"
-        options={{
-          title: "Users",
-        }}
-      />
-      <Tabs.Screen
-        name="chat"
-        options={{
-          title: "Chat",
-          tabBarLabel: "Chat",
-          tabBarBadge: 3,
-        }}
-      />
-    </Tabs>
+    <StoreProvider>
+      <SocketProvider
+        sessionData={sessionData}
+        apiBaseUrl="https://bfxz3hqs-8000.inc1.devtunnels.ms"
+      >
+        <Tabs
+          initialRouteName="map"
+          screenOptions={{
+            headerShown: false,
+            tabBarStyle: {
+              backgroundColor: "#bfdbfe",
+            },
+          }}
+        >
+          <Tabs.Screen
+            name="map"
+            options={{
+              title: "Map",
+            }}
+          />
+          <Tabs.Screen
+            name="users"
+            options={{
+              title: "Users",
+            }}
+          />
+          <Tabs.Screen
+            name="chat"
+            options={{
+              title: "Chat",
+              tabBarLabel: "Chat",
+              tabBarBadge: 3,
+            }}
+          />
+        </Tabs>
+      </SocketProvider>
+    </StoreProvider>
   );
 }
--- apps/mobile/app/(playground)/chat.tsx
+++ apps/mobile/app/(playground)/chat.tsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
 import {
-  Button,
   Keyboard,
   KeyboardAvoidingView,
+  Pressable,
+  ScrollView,
   Text,
   TextInput,
   TouchableWithoutFeedback,
   View,
 } from "react-native";
 import { sessionData } from "@repo/types";
-import { useMessageSession, useSocket } from "@repo/hooks";
+import { useMessageSession, useSocketContext } from "@repo/hooks";
 
 export default function Chat() {
-  const socket = useSocket("https://bfxz3hqs-8000.inc1.devtunnels.ms");
-  // console.log("Socket connected:", socket);
+  const socket = useSocketContext();
   const sessionData: sessionData = {
-    roomId: "test-room",
-    userId: "user-123",
-    username: "JohnDoe",
-    destinationPosition: [37.7749, -122.4194],
-    destinationName: "San Francisco",
+    roomId: "cmht0yeau0000m6rloumdy1iw",
+    userId: "cmht0z3ox0004m6rljv6em956",
+    username: "mobile",
+    destinationPosition: [30.357234, 76.7951835],
+    destinationName: "Jandli, Ambala Cantt, Ambala, Haryana, 132004, India",
   };
   const messages = useMessageSession(socket, sessionData);
   const [messageInput, setMessageInput] = useState("");
@@ -40,27 +40,53 @@ export default function Chat() {
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <KeyboardAvoidingView
         behavior="padding"
-        keyboardVerticalOffset={100}
-        className="w-full flex-1 justify-center bg-slate-100"
+        keyboardVerticalOffset={50}
+        className="w-full flex-1 justify-center"
       >
-        <View className="flex-1 bg-blue-50 ">
-          <Text className="text-2xl p-2">Chat Here</Text>
-          <View className="bg-neutral-300 flex-1 w-full rounded-t-3xl p-4 flex">
-            {messages.map((msg, index) => (
-              <Text key={index} className="pb-2"></Text>
-            ))}
-          </View>
-          <View className="flex flex-row">
-            <View className="flex-grow">
+        <View className="flex-1 bg-white">
+          <Text className="text-2xl p-4 font-bold">Chat Here</Text>
+          <ScrollView className="bg-neutral-50 border border-neutral-300/50 w-full rounded-t-3xl p-4">
+            {messages.map((msg, index) => {
+              const showUsername =
+                msg.userId !== sessionData.userId &&
+                (index === 0 || messages[index - 1].userId !== msg.userId);
+              return (
+                <View
+                  key={msg.id}
+                  className={`flex mt-2 ${msg.userId === sessionData.userId ? "items-end" : "items-start"}`}
+                >
+                  {showUsername && (
+                    <Text className="text-sm mb-1">{msg.username}</Text>
+                  )}
+                  <Text
+                    className={`${
+                      msg.userId === sessionData.userId
+                        ? "bg-blue-400  rounded-tl-2xl rounded-br-2xl"
+                        : "bg-slate-200  rounded-tr-2xl rounded-bl-2xl"
+                    } text-slate-800 text-lg  w-fit max-w-[70%] px-3 py-1 break-words`}
+                  >
+                    {msg.content}
+                  </Text>
+                </View>
+              );
+            })}
+          </ScrollView>
+          <View className="flex flex-row z-10">
+            <View className="flex-1  border border-neutral-200/50">
               <TextInput
                 onChangeText={setMessageInput}
                 onBlur={() => {}}
                 value={messageInput}
                 placeholder="Type your message"
-                className="text-neutral-900 px-4"
+                className="text-neutral-900 px-4 bg-white flex-1"
               />
             </View>
-            <Button title="Send Message" onPress={handleSubmit} />
+            <Pressable
+              onPress={handleSubmit}
+              className="self-center p-4 bg-blue-400"
+            >
+              <Text>Send</Text>
+            </Pressable>
           </View>
         </View>
       </KeyboardAvoidingView>
