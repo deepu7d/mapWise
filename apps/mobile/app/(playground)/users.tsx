bf38dc6c9eefdb29bb51f4db3ed3b4773155c62d
deepu7d <deepanshu.9dd@gmail.com>
2025-11-10 17:31:26 +0530

    add user cards

--- apps/mobile/app/(playground)/users.tsx
+++ apps/mobile/app/(playground)/users.tsx
@@ -1,10 +1,30 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import { useAppSelector } from "@repo/store";
 
 export default function Users() {
+  const users = useAppSelector((state) => state.users);
+  console.log("Users:", users);
   return (
-    <View>
-      <Text>Users</Text>
+    <View className="flex-1 bg-white">
+      <Text className="text-2xl p-4 font-bold">Users</Text>
+      <ScrollView className="bg-neutral-50 border border-neutral-300/50 w-full rounded-t-3xl p-4">
+        {users.map((user) => (
+          <View
+            key={user.id}
+            className="bg-neutral-200 px-4 py-2 mb-4 rounded-2xl"
+          >
+            <Text className="font-bold">{user.name}</Text>
+            <Text
+              className={`text-${user.online ? "green" : "red"}-400 font-bold`}
+            >
+              {user.online ? "Online" : "Offline"}
+            </Text>
+            <Text className="">{user.distance} km</Text>
+            <Text>{user.duration} away</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
