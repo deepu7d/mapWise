22e44d9e6e2d10b5b04653c9943619fdb1662582
Deepanshu <112980826+deepu7d@users.noreply.github.com>
2026-04-27 13:41:51 +0530

    Fix typo in className attribute

--- apps/web/src/app/form/page.tsx
+++ apps/web/src/app/form/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
   return (
     <main className="mx-auto flex h-dvh max-w-xl flex-col items-center justify-center border-gray-200 shadow-md">
       <Suspense fallback={<Loading />}>
-        <p classname="text-red-400">Connection to server may take more than a minute because the backend is deployed on a free Server (Render)</p>
+        <p className="text-red-400">Connection to server may take more than a minute because the backend is deployed on a free Server (Render)</p>
         <MainForm />
       </Suspense>
     </main>
